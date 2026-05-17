4298232b86516c225eb1fd097cbf4afebd1441e2
proxima-602110 <venkat.rohan06@gmail.com>
2026-05-17 15:47:54 +0530

    Updated gpiobasic.c

--- gpiobasic.c
+++ gpiobasic.c
@@ -2,9 +2,9 @@
 
 int main(void){
 	//Address = Base Address + Offset
-	uint32_t *pRCCAHBENR=(uint32_t*)0x40021014; //Enable clock peripheral
-	uint32_t *pPortDMode=(uint32_t*)0x48000C00; //Configure port mode register GPIOx_MODER
-	uint32_t *pPortDOutputReg=(uint32_t*)0x48000C14; //Configure output register GPIOx_ODR
+	volatile uint32_t *const pRCCAHBENR=(uint32_t*)0x40021014; //Enable clock peripheral
+	volatile uint32_t *const pPortDMode=(uint32_t*)0x48000C00; //Configure port mode register GPIOx_MODER
+	volatile uint32_t *const pPortDOutputReg=(uint32_t*)0x48000C14; //Configure output register GPIOx_ODR
 
 	*pRCCAHBENR |=0x00100000;
 	*pPortDMode &=0xFFFFFFCF; //Preserve other bits, clear wanted bits
